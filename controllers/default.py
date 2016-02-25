6bd97964d86839626c7635e80c8599f0bdbbf40c
Christopher Kyle <ckyle@ucsc.edu>
2016-02-25 14:33:36 -0800

    Save using the entered filename

--- controllers/default.py
+++ controllers/default.py
@@ -39,7 +39,7 @@ def ideal_editor():
     return dict(filename=filename.get('name'))
 
 def save_to_server():
-   f = open("test_file", 'w')
+   f = open(request.vars.filename, 'w')
    f.write(request.vars.code)
    f.close
    return dict()
